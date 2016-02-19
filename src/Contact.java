45233e70267de19d00b8c168c354f1d8ae485b4f
BBK <PiJ>
2016-02-19 13:39:49 +0000

    Interfaces copied, pasted and cleaned up.

---
+++ src/Contact.java
@@ -0,0 +1,45 @@
+/**
+ * A contact is a person we are making business with or may do in the future.
+ *
+ * Contacts have an ID (unique, a non-zero positive integer), a name (not
+ * necessarily unique), and notes that the user may want to save about them.
+ */
+
+public interface Contact{
+
+    /**
+     * Returns the ID of the contact.
+     *
+     * @return The ID of the contact.
+     */
+
+    int getId();
+
+    /**
+     * Returns the name of the contact.
+     *
+     * @return The name of the contact.
+     */
+
+    String getName();
+
+    /**
+     * Returns our notes about the contact, if any.
+     *
+     * If we have not written anything about the contact, the empty string is
+     * returned.
+     *
+     * @return A string with notes about the contact, maybe empty.
+     */
+
+    String getNotes();
+
+    /**
+     * Add notes about the contact.
+     *
+     * @param note The notes to be added.
+     */
+
+    void addNotes(String note);
+
+}
